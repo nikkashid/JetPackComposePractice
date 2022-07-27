7517cb749b5625e691067d86de7b88498d30c22c
nikhil.kashid1@gmail.com <Shinigami1408>
2022-07-27 12:41:24 +0530

    Adding Toast

--- app/src/main/java/com/nikhil/myapplication/MainActivity.kt
+++ app/src/main/java/com/nikhil/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.nikhil.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,9 +43,12 @@ fun Greeting(name: String) {
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val context = LocalContext.current
         Text(text = "Hello", fontSize = 28.sp)
         Spacer(modifier = Modifier.height(40.dp))
-        Text(text = "$name!", fontSize = 28.sp)
+        Text(text = "$name!", fontSize = 28.sp, modifier = Modifier.clickable {
+            Toast.makeText(context, "Clicked Text", Toast.LENGTH_LONG).show()
+        })
         Spacer(modifier = Modifier.height(40.dp))
         Text(text = "Greetings", fontSize = 28.sp)
     }
